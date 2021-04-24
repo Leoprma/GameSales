1c84949f1333f22e969f81da833bd5a24488cfef
Leonardo Prata <lpratamaciel@gmail.com>
2021-04-23 21:37:31 -0300

    Final Version

--- Game_recommendation.py
+++ Game_recommendation.py
@@ -1,37 +1,48 @@
 import pandas as pd
-import rawgpy
+import requests
+from bs4 import BeautifulSoup
 import streamlit as st
 
 st.title("Game Recommendation")
 st.header('This is a platform that suggests games based on the one input by the user')
 
-user_input = st.text_input("Game Title", "Search")
-rawg = rawgpy.RAWG("User-Agent, this should identify your app")
-results=rawg.search(user_input)
-slug=results[0].slug
-suggestions=pd.json_normalize(list(rawg.game_suggestions(slug))[0:3])
+try:
+	user_input = st.text_input("Game Title", "Search")
+	game_name = user_input.lower().replace(' ','+')
+	requests.get(f'https://api.rawg.io/api/games?key=f9ffc69c548c4547bdfe188c29fa4086&search={game_name}').json()
+	slug=pd.json_normalize(requests.get(f'https://api.rawg.io/api/games?key=f9ffc69c548c4547bdfe188c29fa4086&search={game_name}').json())['results'][0][0]['slug']
+	soup=BeautifulSoup(requests.get(f'https://rawg.io/games/{slug}/suggestions').content)
+	game_name=[]
+	metacritic=[]
+	release=[]
+	genre=[]
+	description=[]
+	background_image=[]
+	for i in range(3):
+		game_name.append(soup.find_all('a',attrs={'class':"game-card-compact__heading game-card-compact__heading_with-link"})[i].text)
+		metacritic.append(soup.find_all('div',attrs={'class':"metascore-label metascore-label_green"})[i].text)
+		description.append(soup.find_all('div',attrs={'class':"truncate-block game-card__about-text"})[i].text)
+	for i in range(1,13,4):
+		release.append(soup.find_all('div',attrs={'class':"game-card-about__desription"})[i].text)
+		genre.append(soup.find_all('div',attrs={'class':"game-card-about__desription"})[i+1].text)
+	for i in range(3):
+		suggestion_name=game_name[i].lower().replace(' ','+')
+		game_info=requests.get(f'https://api.rawg.io/api/games?key=f9ffc69c548c4547bdfe188c29fa4086&search={suggestion_name}').json()
+		background_image.append(game_info['results'][0]['background_image'])
 #st.dataframe(suggestions)
 
-for i in range(3):
-	st.write(f'**- Suggestion {i+1}:**\n')
-	st.write('**Name:**\n')
-	st.write(f"{suggestions['name'][i]}\n")
-	st.image(suggestions['background_image'][i],use_column_width=True)
-	st.write('**About:**\n')
-	st.write(suggestions['short_description'][i])
-	genres=[]
-	for j in range(len(suggestions['genres'][i])):
-		genres.append(suggestions['genres'][i][j]['name'])
-	st.write('**Genres:**\n')
-	st.write(f"{genres}\n")
-	st.write('**Release date:**\n')
-	st.write(f"{suggestions['released'][i]}\n")
-	st.write('**Metacritic score:**\n')
-	st.write(f"{suggestions['metacritic'][i]}\n")
-	parent_platform=[]
-	for k in range(len(suggestions['parent_platforms'][i])):
-		parent_platform.append(suggestions['parent_platforms'][i][k]['platform']['name'])
-	st.write("**Platforms:**\n")
-	st.write(f'{parent_platform}\n')
-	st.write("**Estimated time to beat the game:**\n")
-	st.write(f"{suggestions['playtime'][i]} hours\n")
+	for i in range(3):
+		st.write(f'**- Suggestion {i+1}:**\n')
+		st.write('**Name:**\n')
+		st.write(f"{game_name[i]}\n")
+		st.image(background_image[i],use_column_width=True)
+		st.write('**About:**\n')
+		st.write(description[i])
+		st.write('**Genres:**\n')
+		st.write(f"{genre[i]}\n")
+		st.write('**Release date:**\n')
+		st.write(f"{release[i]}\n")
+		st.write('**Metacritic score:**\n')
+		st.write(f"{metacritic[i]}\n")
+except:
+	pass
